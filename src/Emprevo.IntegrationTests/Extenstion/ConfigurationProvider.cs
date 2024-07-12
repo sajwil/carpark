4fa3a84f90be47f62cb0ab9272ae6da718227251
sajwil <sajwil@outlook.com>
2024-07-12 14:06:08 +1000

    Added Integration tests

---
+++ src/Emprevo.IntegrationTests/Extenstion/ConfigurationProvider.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Emprevo.IntegrationTests.Extenstion
+{
+    public static class ConfigurationProvider
+    {
+        public static IConfiguration GetConfiguration()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            return configuration;
+        }
+    }
+}
